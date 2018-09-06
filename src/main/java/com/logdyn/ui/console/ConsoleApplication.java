04f01cf4526f420ac5ade8e87fc873c31116dba0
jsjlewis96 <jsjlewis96@gmail.com>
2018-09-06 20:15:50 +0100

    Reworked program entry points

---
+++ src/main/java/com/logdyn/ui/console/ConsoleApplication.java
@@ -0,0 +1,59 @@
+package com.logdyn.ui.console;
+
+import com.logdyn.ui.console.commands.Command;
+import com.logdyn.ui.console.commands.ExitCommand;
+import com.logdyn.ui.console.commands.HelpCommand;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class ConsoleApplication {
+
+    private static final Logger LOGGER = Logger.getLogger(ConsoleApplication.class);
+    private static final Collection<Command> commands = new ArrayList<>();
+    private static Command HELP_COMMAND = new HelpCommand(commands);
+    private static String VERSION = "0.5.0";
+
+    static {
+        //TODO add commands
+        commands.add(HELP_COMMAND);
+        commands.add(new ExitCommand());
+    }
+
+    public static void main(final String... args) {
+        if (args[0].equals("-i")) {
+            interactive();
+        } else {
+            execute(args);
+        }
+    }
+
+    private static void interactive() {
+        System.out.println(String.format("Keeper: v%s", VERSION));
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                System.out.print("> ");
+                final String[] args = reader.readLine().split("\\s");
+                execute(args);
+            }
+        } catch (final IOException e) {
+            LOGGER.error("Error reading from system input", e);
+        }
+    }
+
+    private static void execute(final String[] args) {
+        commands.stream()
+                .filter(command -> command.getName().equalsIgnoreCase(args[0]))
+                .findAny()
+                .orElse(HELP_COMMAND)
+                .execute(Arrays.copyOfRange(args, 1, args.length));
+    }
+}
--- src/main/java/com/logdyn/ui/javafx/FxApplication.java
+++ src/main/java/com/logdyn/ui/javafx/FxApplication.java
@@ -2,10 +2,15 @@ package com.logdyn.ui.javafx;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
 public class FxApplication extends Application {
+
+    private static final Logger LOGGER = Logger.getLogger(FxApplication.class);
+
     @Override
     public void start(final Stage primaryStage) throws Exception {
-
+        LOGGER.debug("Starting JavaFX Application...");
+        this.stop();
     }
 }
