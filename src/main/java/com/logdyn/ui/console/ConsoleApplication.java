ff01d953c2ff50ce9ee9d55af77e23c8a9822c71
jsjlewis96 <jsjlewis96@gmail.com>
2018-09-13 21:18:25 +0100

    Picocli now uses log4 logging

--- src/main/java/com/logdyn/ui/console/ConsoleApplication.java
+++ src/main/java/com/logdyn/ui/console/ConsoleApplication.java
@@ -6,10 +6,20 @@ import com.logdyn.ui.console.commands.repository.RepositoryAddCommand;
 import com.logdyn.ui.console.commands.repository.RepositoryCommand;
 import com.logdyn.ui.console.commands.repository.RepositoryListCommand;
 import com.logdyn.ui.console.commands.repository.RepositoryRemoveCommand;
+import org.apache.log4j.Logger;
 import picocli.CommandLine;
+import picocli.CommandLine.ExecutionException;
+import picocli.CommandLine.IExceptionHandler2;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.ParseResult;
 import picocli.CommandLine.RunAll;
 
+import java.util.List;
+
 public class ConsoleApplication {
+
+    private static Logger LOGGER = Logger.getLogger(ConsoleApplication.class);
+
     public static void main(final String... args) {
         CommandLine commandLine = new CommandLine(new KeeperCommand())
                 .addSubcommand("repository", new CommandLine(new RepositoryCommand())
@@ -18,6 +28,22 @@ public class ConsoleApplication {
                         .addSubcommand("list", new RepositoryListCommand(), "-l"),
                         "repos")
                 .addSubcommand("exit", new ExitCommand());
-        commandLine.parseWithHandler(new RunAll(), args);
+        commandLine.parseWithHandlers(new RunAll(), new CliExceptionHandler(), args);
     }
+
+    static class CliExceptionHandler implements IExceptionHandler2<List<Object>> {
+        @Override
+        public List<Object> handleParseException(final ParameterException ex, final String[] args) {
+            LOGGER.debug(ex.getMessage(), ex);
+            return null;
+        }
+
+        @Override
+        public List<Object> handleExecutionException(final ExecutionException ex, final ParseResult parseResult) {
+            LOGGER.error(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
 }
+
