bd1f70556cc723f8612c2efee2f9379df01d2b77
Matt Rayner <me@mattihew.com>
2019-03-30 18:24:30 +0000

    enabled ansi output on windows 10

--- src/main/java/com/logdyn/Main.java
+++ src/main/java/com/logdyn/Main.java
@@ -3,17 +3,10 @@ package com.logdyn;
 
 import com.logdyn.core.authentication.ssl.InteractiveTrustManager;
 import com.logdyn.core.persistence.StorageController;
-import com.logdyn.ui.console.commands.ExitCommand;
 import com.logdyn.ui.console.commands.KeeperCommand;
-import com.logdyn.ui.console.commands.repository.RepositoryAddCommand;
-import com.logdyn.ui.console.commands.repository.RepositoryCommand;
-import com.logdyn.ui.console.commands.repository.RepositoryListCommand;
-import com.logdyn.ui.console.commands.repository.RepositoryRemoveCommand;
-import com.logdyn.ui.javafx.FxApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -57,6 +50,10 @@ public class Main implements CommandLineRunner
         addSSLVerifier(new InteractiveTrustManager());
         StorageController.load();
         Runtime.getRuntime().addShutdownHook(new Thread(StorageController::save, "Shutdown Save Hook"));
+        if ("Windows 10".equalsIgnoreCase(System.getProperty("os.name")))
+        {
+            System.setProperty("picocli.ansi", "true");
+        }
     }
 
     @Override
